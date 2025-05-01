8ba87fb89f520b83bff8e154ee870c29c3372682
Sofiane CHADILI <sofiane.chadili@hotmail.com>
2025-05-01 11:37:00 +0200

    first commit

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,6 @@
-import './bootstrap.js';
+// Commentons cette ligne qui cause l'erreur
+// import './bootstrap.js';
+
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -6,5 +8,10 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+// ...
+import { createApp } from 'vue';
+import App from './components/App.vue';
+
+createApp(App).mount('#app');
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,2 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
-
-const app = startStimulusApp();
-// register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
+// Fichier bootstrap.js simplifié sans dépendance à stimulus-bundle
+console.log('Bootstrap file loaded');
